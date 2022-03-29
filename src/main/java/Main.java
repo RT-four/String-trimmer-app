ed6837516394ed3661c32d07235087be6c49bea2
Philipp <philvas13@gmail.com>
2022-03-29 19:20:25 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Trimmer started, enter your command:");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+//        cut (-c|-w) ((-o (.+) )|())(((.+) )|())range (([0-9]+-[0-9]+)|(-[0-9]+)|([0-9]+-))
+    }
+}
