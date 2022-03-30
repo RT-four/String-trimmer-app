bff4b0fe3099a1dbda8b61b5e1170fd94b37b7dd
Philipp <philvas13@gmail.com>
2022-03-30 11:13:14 +0300

    rework classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Trimmer started, enter your command:");
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
         Trimmer trimmer = new Trimmer();
-        trimmer.start(command);
+        trimmer.start();
+        System.out.println("df");
     }
 }
--- src/main/java/Trimmer.java
+++ src/main/java/Trimmer.java
@@ -1,10 +1,14 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Trimmer {
     private final String CommandPattern = "cut (-c|-w) ((-o ([^ ]+) )|())((([^ ]+) )|())range (([0-9]+-[0-9]+)|(-[0-9]+)|([0-9]+-))";
     private Command command;
 
-    public void start(String input) throws Exception {
+    public void start() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Trimmer started, enter your command:");
+        String input = scanner.nextLine();
         if (!input.matches(CommandPattern)) {
             throw new Exception("Illegal command");
         }
